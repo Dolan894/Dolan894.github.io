2df72e9870af29865502aca16d434459ec9dea26
Dolan894 <53367674+Dolan894@users.noreply.github.com>
2019-08-27 00:55:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);
 hellopreloader.style.display = "none";}},16);}
 window.onload = function()
   {setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
+
+$('.link').click(function(){
+      var volume = $(this);
+      volume.toggleClass('on');
+      if (volume.is('.on')) $('#video').prop("volume", 1);
+      else $('#video').prop("volume", 0);
+   });
